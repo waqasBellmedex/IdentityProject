a427e315abdef18fdd39694bd553499e6ae8f976
waqas.mumtaz <waqas.mumtaz@WAQAS-MUMTAZ>
2024-12-16 09:34:23 +0500

    Add signalR project

---
+++ signalRLib/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace signalRLib
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSignalRService(this IServiceCollection service,IConfiguration configuration)
+        {
+            service.AddSignalRCore();
+
+            return service;
+        }
+
+    }
+}
---
+++ signalRLib/Hubs/NotificationHub.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace signalRLib.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public NotificationHub(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+
+    }
+}
